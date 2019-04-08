55841eae30e22c5ba4ce94dcee0a26e812f74d1c
unknown <ulyana250188@gmail.com>
2019-04-09 01:05:19 +0300

    added few animation to scroll and created animetion SVG

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -6,7 +6,8 @@ let linkAbout=document.getElementById('nav-about');
 let linkContact=document.getElementById('nav-contact');
 let overlay=document.getElementById('burger-menu-overlay');
 button.onclick = function() {
-    menu.classList.toggle('burger-menu-active');   
+    menu.classList.toggle('burger-menu-active'); 
+   
 };
 
 linkHome.onclick = function() {
@@ -16,7 +17,8 @@ linkProject.onclick = function() {
     menu.classList.toggle('burger-menu-active');   
 };
 linkAbout.onclick = function() {
-    menu.classList.toggle('burger-menu-active');   
+    menu.classList.toggle('burger-menu-active'); 
+      
 };
 linkContact.onclick = function() {
     menu.classList.toggle('burger-menu-active');   
