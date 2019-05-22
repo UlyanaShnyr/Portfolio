8a7d946b8b6433e67c99af5ec52012863744676b
unknown <ulyana250188@gmail.com>
2019-05-22 19:57:09 +0300

    filled information about myself

--- js/animation_scroll.js
+++ js/animation_scroll.js
@@ -12,7 +12,81 @@ $(window).scroll(function(){
         const imagePos = $(this).offset().top;
         const topOfWindow = $(window).scrollTop();
         if(imagePos < topOfWindow+window.innerHeight){
+            $(this).addClass('fadeInLeft');
+        }
+    })
+
+    $('.movfadeInRight').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){
             $(this).addClass('fadeInRight');
         }
     })
+
+    $('.percentages90').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('html');
+        }
     })
+
+    $('.percentages80').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('css');
+        }
+    })
+
+    $('.percentages75').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('js');
+        }
+    })
+
+    $('.percentages40').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('canvas');
+        }
+    })
+
+    $('.percentages60').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('jquery');
+        }
+    })
+
+    $('.percentages65').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('react');
+        }
+    })
+
+    $('.percentages55').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('sql');
+        }
+    })
+
+    $('.percentages70').each(function(){
+        const imagePos = $(this).offset().top;
+        const topOfWindow = $(window).scrollTop();
+        if(imagePos < topOfWindow+window.innerHeight){            
+            $(this).addClass('photoshop');
+        }
+    })
+    })
+
+   
